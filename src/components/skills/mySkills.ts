23bb3806c720f65b8ad5ad14a8d89bee5bb2949a
Tristan Teufel <tristanteufel@googlemail.com>
2025-01-22 08:34:02 +0100

    refactor

---
+++ src/components/skills/mySkills.ts
@@ -0,0 +1,102 @@
+import {
+    bundlers,
+    databases,
+    ide,
+    integration,
+    languages,
+    libraries,
+    projectMethology,
+    protocols,
+    styling,
+    testing,
+  } from './technologies';
+
+export const mySkills = [
+    {
+      title: "languages",
+      skills: [
+        languages.typescript,
+        languages.javascript,
+        languages.nodejs,
+        languages.python,
+        languages.java,
+        languages.go,
+        languages.csharp,
+      ],
+    },
+    {
+      title: "libraries",
+      skills: [
+        libraries.react,
+        libraries.solid,
+        libraries.gatsby,
+        libraries.astro,
+        libraries.svelte,
+        libraries.angular,
+        libraries.vue,
+        libraries.nextjs,
+        libraries.nx,
+      ],
+    },
+    {
+      title: "styling",
+      skills: [
+        styling.css,
+        styling.scss,
+        styling.mui,
+        styling.styled,
+        styling.tailwind,
+      ],
+    },
+    {
+      title: "testing",
+      skills: [
+        testing.testingLibrary,
+        testing.jest,
+        testing.cypress,
+        testing.playwright,
+      ],
+    },
+    {
+      title: "databases",
+      skills: [databases.mongodb, databases.sql],
+    },
+    {
+      title: "integration",
+      skills: [
+        integration.aws,
+        integration.azure,
+        integration.docker,
+        integration.gitlab,
+        integration.github,
+        integration.linux,
+      ],
+    },
+    {
+      title: "bundlers",
+      skills: [bundlers.vite, bundlers.webpack],
+    },
+    {
+      title: "protocols",
+      skills: [
+        protocols.rest,
+        protocols.jsonrpc,
+        protocols.websocket,
+        protocols.openapi,
+        protocols.graphql,
+      ],
+    },
+    {
+      title: "ide",
+      skills: [ide.intellij, ide.vscode],
+    },
+    {
+      title: "projectMethology",
+      skills: [
+        projectMethology.scrum,
+        projectMethology.kanban,
+        projectMethology.agile,
+        projectMethology.waterfall,
+      ],
+    },
+  ];
